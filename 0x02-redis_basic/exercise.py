75320d90c844c3343a395bda45448dc86cba4c57
Leul <leulzewduchanie@gmail.com>
2025-07-10 13:40:29 +0300

    added exercise file basic redis connecting

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,13 @@
+import redis
+import uuid
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    def store(self,data):
+          _uuid = uuid.uuid4()
+          self._redis.set(str(_uuid),data)
+          return str(_uuid)
+        
+
