dabfef348e6462d5ff12590b18c1317616d80dda
Leul <leulzewduchanie@gmail.com>
2025-07-10 17:03:38 +0300

    added type conversion on client side

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,24 +4,29 @@ A module for caching data in Redis.
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union,Callable,Optional
 
 
 class Cache:
-    """
-    A class for caching data in Redis.
-    """
     def __init__(self) -> None:
-        """
-        Initializes the Cache object and flushes the Redis database.
-        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Stores data in Redis with a random key and returns the key.
-        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    def get(self,key:str,fn:Optional[Callable]=None) -> Union[str,bytes,int,float,None]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+    def get_str(self,key:str) -> Union[str,None]:
+        return self.get(key,fn=lambda d: d.decode("utf-8"))
+    def get_int(self,key:str) -> Union[int,None]:
+        return self.get(key,fn=int)
+
+
+
