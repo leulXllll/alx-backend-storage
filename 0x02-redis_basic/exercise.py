e7d11081a84be74dda911675f7ca18b12bd629c2
Leul <leulzewduchanie@gmail.com>
2025-07-10 14:11:02 +0300

    added type checking and deleted main.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import redis
 import uuid
+from typing import Union
 
 class Cache:
-    def __init__(self):
+    def __init__(self)->None:
         self._redis = redis.Redis()
         self._redis.flushdb()
-    def store(self,data):
+    def store(self,data: Union[str,bytes,int,float])-> str:
           _uuid = uuid.uuid4()
           self._redis.set(str(_uuid),data)
           return str(_uuid)
